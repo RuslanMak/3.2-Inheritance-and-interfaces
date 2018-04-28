880bc5dc2c00525bf4ce7b21f40e6a86ca19c67d
RuslanMak <makarovemd@gmail.com>
2018-04-28 19:07:55 +0300

    version-2

--- inheritance.php
+++ inheritance.php
@@ -20,136 +20,126 @@
  *
  */
 
-//-------------------------------------------Classes-------------------------------------------
-class CarClass
-{
-    public $name;
-    private $color = 'White';
-    public $price;
+//-------------------------------------------MyInterface-------------------------------------------
 
+interface MyConstruct
+{
+    public function __construct($name, $discount, $price);
+}
+
+interface MyPrice extends MyConstruct
+{
+    public function getPrice();
+}
+
+interface ChangeColor
+{
+    public function changeColor($color);
+}
+
+//-------------------------------------------MySuperClass-------------------------------------------
+
+class Products //implements MyConstruct   //--A так можно? Почему "discount, price" тогда подсвечуются(Это плохо)????
+{
+    public function getPrice()
+    {
+        if ($this->discount) {
+            return round($this->price - ($this->price * $this->discount / 100));
+        } else {
+            return $this->price;
+        }
+    }
+
+    public function __construct($name, $discount, $price)
+    {
+        $this->name = $name;
+        $this->discount = $discount;
+        $this->price = $price;
+    }
+}
+
+//-------------------------------------------Classes-------------------------------------------
+class CarClass extends Products implements MyPrice, ChangeColor
+{
+    private $color = 'White';
     public function changeColor($color)
     {
         $this->color = $color;
     }
 }
 
-class TV
+class TV extends Products implements MyPrice
 {
-    private $price;
-    public function __construct($name, $diameter, $discount, $price)
-    {
-        $this->name = $name;
-        $this->diameter = $diameter;
-        $this->discount = $discount;
-        $this->price = $price;
-    }
-
-    public function getPrice()
-    {
-        if ($this->discount) {
-            return round($this->price - ($this->price * $this->discount / 100));
-        } else {
-            return $this->price;
-        }
-    }
+public $diagonal;
 }
 
-class BallPen
+class BallPen extends Products implements MyPrice
 {
-    private $price;
-    public function __construct($name, $color, $discount, $price)
-    {
-        $this->name = $name;
-        $this->color = $color;
-        $this->discount = $discount;
-        $this->price = $price;
-    }
-
-    public function getPrice()
-    {
-        if ($this->discount) {
-            return round($this->price - ($this->price * $this->discount / 100));
-        } else {
-            return $this->price;
-        }
-    }
+    public $color;
 }
 
-class Duck
+class Duck extends Products implements MyPrice
 {
-    public function __construct($name, $color, $price)
-    {
-        $this->name = $name;
-        $this->color = $color;
-        $this->price = $price;
-    }
+    public $color;
 }
 
-class Product
+class Product extends Products implements MyPrice
 {
-    private $price;
-    public function __construct($category, $name, $discount, $price)
-    {
-        $this->category = $category;
-        $this->name = $name;
-        $this->discount = $discount;
-        $this->price = $price;
-    }
-
-    public function getPrice()
-    {
-        if ($this->discount) {
-            return round($this->price - ($this->price * $this->discount / 100));
-        } else {
-            return $this->price;
-        }
-    }
+    public $category;
 }
 
 //------------------------------------- Objects---------------------------------------------
 //----------------CAR----------------
-$bmw = new CarClass();
-$bmw->name = "BMW";
-$bmw->new = "NEW - Mожно ли так создавать новое свойство?";  // --- Mожно ли так создавать новое свойство???
-$bmw->price = 1100;
+$bmw = new CarClass('BMW', '10', 700);
 $bmw->changeColor('Blue');
 
-$audi = new CarClass();
-$audi->name = "AUDI";
-$audi->price = 900;
+$audi = new CarClass('AUDI', '10', 900);
 
 //----------------TV-----------------
-$lg = new TV('LG', 24, '', 500);
-$sone = new TV('SONE', 42, '10', 700);
+$lg = new TV('LG', '', 500);
+$lg->diagonal = 42;
+
+$sone = new TV('SONE', '10', 700);
+$sone->diagonal = 80;
 
 //----------------PEN-----------------
-$parker = new BallPen('Parker', 'Black', '', 20);
-$space_pen = new BallPen('Space Pen', 'Black', '', 70);
+$parker = new BallPen('Parker', '10', 20);
+$parker->color = 'Black';
+
+$space_pen = new BallPen('Space Pen', 'Black', 70);
+$space_pen->color = 'Black';
 
 //----------------Duck-----------------
-$american_pekin = new Duck('American Pekin', 'White', 10);
-$american_black_duck = new Duck('American black duck', 'Black', 11);
+$american_pekin = new Duck('American Pekin', 10, 10);
+$american_pekin->color = 'White';
 
-//----------------TV-----------------
-$cup = new Product('Cups', 'White cup', '', 2);
-$book = new Product('Books', 'Health', '', 5);
+$american_black_duck = new Duck('American black duck', '10', 11);
+$american_black_duck->color = 'Black';
+
+//----------------Product-----------------
+$cup = new Product('White cup', '10', 2);
+$cup->category = 'Cups';
+
+$book = new Product('Health', '', 5);
+$book->category = 'Books';
 
 
-//-----------------------------------Echo-----------------
+//-----------------------------------Echo------------------------------------------------------
+echo "<h2>Car</h2>";
 echo "<pre>";
 print_r($bmw);
-echo "<br>";
+echo "Price for " . "$bmw->name: " . $bmw->getPrice() . "<br><br><br>";
 
 print_r($audi);
-echo "<br><br><br><br><br><br>";
+echo "Price for " . "$audi->name: " . $audi->getPrice() . "<br><br><br>";
 
 //------------TV--------------
+echo "<h2>TV</h2>";
 print_r($lg);
 echo "Price for " . "$lg->name: " . $lg->getPrice() . "<br><br><br>";
 
 print_r($sone);
 echo "Price for " . "$sone->name: " . $sone->getPrice() . "<br>";
-
 
 
 
